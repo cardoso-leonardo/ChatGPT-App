28910e0a8b08d5895be013921e1a00c8a5cead9e
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-04-18 21:47:13 -0300

    feat: implement ChatView initializers and tableView

--- ChatGPT-App/ChatGPT-App/Controller/ChatViewController.swift
+++ ChatGPT-App/ChatGPT-App/Controller/ChatViewController.swift
@@ -9,40 +9,23 @@ import UIKit
 
 class ChatViewController: UIViewController {
     
-    var button: UIButton = {
-        let button = UIButton(configuration: .filled())
-        button.setTitle("Test Button", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
     
-    @objc
-    func buttonTapped(sender: UIButton!) {
-        Service.shared.sendMessage(text: "Olá, como você tá?") { result in
-            switch result {
-            case .success(let success):
-                print(success)
-            case .failure(let failure):
-                print(failure)
-            }
-        }
-    }
+    private let chatView = ChatView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         view.backgroundColor = .systemBackground
-        view.addSubview(button)
+        view.addSubview(chatView)
         addConstraints()
     }
     
     private func addConstraints() {
         NSLayoutConstraint.activate([
-            button.heightAnchor.constraint(equalToConstant: 50),
-            button.widthAnchor.constraint(equalToConstant: 250),
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            chatView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            chatView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            chatView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            chatView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
             
         ])
     }
--- ChatGPT-App/ChatGPT-App/View/ChatView.swift
+++ ChatGPT-App/ChatGPT-App/View/ChatView.swift
@@ -13,9 +13,28 @@ final class ChatView: UIView {
         let tableView = UITableView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = UIColor.systemPink
         return tableView
     }()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(tableView)
+        addConstraints()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
 
 }
