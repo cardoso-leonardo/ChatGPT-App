d27df0a8646bce81f8fd36474cd7371c7fe565ec
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-04-20 18:46:59 -0300

    feat: create ChatCellView

---
+++ ChatGPT-App/ChatGPT-App/View/ChatCellView.swift
@@ -0,0 +1,23 @@
+//
+//  ChatTableViewCell.swift
+//  ChatGPT-App
+//
+//  Created by Leonardo Cardoso on 20/04/23.
+//
+
+import UIKit
+
+class ChatCellView: UITableViewCell {
+    
+    public static let identifier = "ChatCellView"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
