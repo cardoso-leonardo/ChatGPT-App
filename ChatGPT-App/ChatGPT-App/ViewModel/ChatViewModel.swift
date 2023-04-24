fe12ee3f0a5acd244c575887e88d3f61a3a4aef1
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2023-04-24 19:38:01 -0300

    feat: add UITableView delegate and datasource protocols to ChatViewModel

--- ChatGPT-App/ChatGPT-App/ViewModel/ChatViewModel.swift
+++ ChatGPT-App/ChatGPT-App/ViewModel/ChatViewModel.swift
@@ -5,8 +5,19 @@
 //  Created by Leonardo Cardoso on 17/04/23.
 //
 
-import Foundation
+import UIKit
 
-final class ChatViewModel {
+final class ChatViewModel: NSObject {
+    
+}
+extension ChatViewModel: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     
 }
